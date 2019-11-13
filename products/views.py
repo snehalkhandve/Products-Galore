6dd39ed94ff4b65ab74b07f8df2a6ab7f3a13ded
snehalkhandve <52536837+snehalkhandve@users.noreply.github.com>
2019-11-13 10:43:44 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product # to import all the Product code 
+from .models import Product
 from django.utils import timezone
 
 # Create your views here.
 
 def home(request):
-	products = Product.objects # get all the available products from database
+	products = Product.objects 
 	return render(request, 'products/home.html', {'products':products})
 
-# django is just sooooo amazing that with mere single statement below it will 1st check if user is logged in 
-# and allow access to create page else redirect it to login page
 @login_required(login_url="/accounts/signup")
 def create(request):
 	if request.method == 'POST':
 		if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['icon'] and request.POST['image'] :
-			product = Product() # create a new product object for owner who wanna create a product
+			product = Product()
 			product.title = request.POST['title']
 			product.body = request.POST['body']
 			if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://') :
@@ -26,7 +24,7 @@ def create(request):
 			product.image = request.POST['image']
 			product.pub_date = timezone.datetime.now()
 			product.hunter = request.user
-			product.save() # to save the product
+			product.save() 
 			return redirect('/products/'+ str(product.id))
 		else:
 			return render(request , 'products/create.html', {'error':'All the fields should be filled'})
@@ -34,19 +32,14 @@ def create(request):
 		return render(request, 'products/create.html')
 
 
-'''
-	So whenever u r adding images in create.html page 1st create an images folder inside producthunt-project
-	and add images there then choose images from these images folder only
-'''
-
 def  detail(request , product_id):
 	product = get_object_or_404(Product, pk = product_id)
 	return render(request, 'products/detail.html', {'product': product})
 
-@login_required(login_url="/accounts/signup")  # u can upvote only when logged in
+@login_required(login_url="/accounts/signup")  
 def  upvote(request, product_id):
 	if request.method == 'POST':
 		product = get_object_or_404(Product, pk = product_id)
 		product.votes_total += 1
-		product.save() 		# save this to database
+		product.save() 		
 		return redirect('/products/'+ str(product.id))
