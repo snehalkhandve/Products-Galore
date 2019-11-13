dcc1b96e6553456ed97470c5a5076e7dafd47a91
snehalkhandve <52536837+snehalkhandve@users.noreply.github.com>
2019-11-13 10:42:04 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# we dont have the model for user that we created but django does so we just include it
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -12,21 +11,14 @@ class Product(models.Model):
 	image = models.ImageField(upload_to='images/')
 	icon = models.ImageField(upload_to='images/')
 	votes_total = models.IntegerField(default=1)
-	# hunter is a person who has submitted or found the product
 	hunter = models.ForeignKey(User, on_delete = models.CASCADE)
-	# so here we are assigning an id to hunter and this id will be a foreignkey referencing the user table 
-	# and we have used ON DELETE CASCADE in case of any deletes
-
+	
 	def __str__(self):
 		return self.title
 
 	def summary(self):
-	 	return self.body[:100] # slicing body and taking 1st 100 chars only
+	 	return self.body[:100] 
 
 	def pub_date_pretty(self):
 		return self.pub_date.strftime('%b %e %Y')
 
-
-# no do :
-# python manage.py makemigrations
-# python manage.py migrate
